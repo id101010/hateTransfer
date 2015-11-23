070681bb7665b5710a7b281d4ac7d4961a2dca39
id101010 <aaron@duckpond.ch>
2015-11-23 02:17:58 +0100

    Major overhaul, added more checks, cleaned, added colour <3 :octocat:

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # hateTransfer
-mp3fs driven death metal music uploader for android devices
+
+![Example use](https://github.com/id101010/hateTransfer/blob/master/doc/screen.png)
+
+A little skript which helps you to convert and upload lossless music to your android phone in one single step!
 
 ## Synopsis
 Got a lossless music collection? Ever wanted to upload mp3 versions of your music to your phone due to limited storage? Thanks to [mp3fs](https://khenriks.github.io/mp3fs/) (check it out, awesome project!) theres a solution without keeping two separated music libraries.
+If you're wondering about the name, it's kind of intendet to get death metal on my phone, if you don't like it, change it. I don't care. ;)
 
-Yes, this script is strictly intended for death metal, hence the name. ;)
+## Main Features
+You're not sure if you already uploaded something? Nevermind, the script will handle it!
+The needed space on the phone will be calculatet before uploading aswell, so there are no bad surprises during the upload.
 
 ## Usage
-hateTransfer [list of music to transfer]
+Basically just throw a bunch of folders in there and the script will handle the rest. 
 
 e.g.: $ hateTransfer "Six Feet Under" Aborted Acranius "God Dethroned"
 
 ## Requirements
-On Android Phone (Tested on cyanogenmod 12+)
+Android phone
 * busybox
 
-On Computer
+Computer
 * adb
 * mp3fs
--- hateTransfer.sh
+++ hateTransfer.sh
@@ -1,65 +1,104 @@
 #!/bin/bash
 #
-# Transfers all your finest death metal to your phone!
+# Convert and transfer your FLAC music to an android phone in one single step!
 #
-# Usage: hateTransfer [List of Music]
+# The script assumes that you have a music library of the following form:
+# $(MUSIC_DIR)/Interpret/Album
+#
+# Example: hateTransfer "Swallow the Sun" "Spawn of Possession" ...
 #
 
 # Some options
-QUALITY='-b 320 --quality=0'
+QUALITY="-b 320 --quality=0"
 MP3LIB="/home/${USER}/mp3fs/"
 FLACLIB="/home/${USER}/Music/"
-ANDROIDLIB='/mnt/sdcard/Music/'
+ANDROIDLIB="/mnt/sdcard/Music/"
 
-# Create mp3fs mountpoint if not exists
+# Some useful variables
+NOT_PRESENT="List of devices attached"
+ADB_FOUND=$(adb devices | tail -2 | head -1 | cut -f 1 | sed 's/ *$//g')
+
+# Create mp3fs mountpoint if it isn't already there ---------------------------------
 if [ ! -d ${MP3LIB} ]
 then
     mkdir ${MP3LIB}
 fi
 
-# Check if mp3fs is running.
+# Check if mp3fs is running ---------------------------------------------------------
 if [ -z "$(pgrep mp3fs)" ]
 then
+    echo -e "[\033[32m:)\033[m] starting mp3fs."
     mp3fs ${QUALITY} ${FLACLIB} ${MP3LIB}
 else
-    echo "[DEBUG]: mp3fs is running."
+    echo -e "[\033[32m:)\033[m] mp3fs is running."
 fi
 
-# Check if adb is running.
+# Check if adb is running -----------------------------------------------------------
 if [ -z "$(pgrep adb)" ]
 then
     adb start-server
 else
-    echo "[DEBUG]: adb server is running."
+    echo -e "[\033[32m:)\033[m] adb server is running."
 fi
 
-# Perform some error checks and upload the music to the phone
+# Check if any android device is connected ------------------------------------------
+if [ "${ADB_FOUND}" == "${NOT_PRESENT}" ] 
+then
+	echo -e "[\033[31m:(\033[m] Android device seems to be missing."
+	exit 1
+else
+	echo -e "[\033[32m:)\033[m] Android device ${ADB_FOUND} found."
+fi
+
+# Check if each folder exists in your library ----------------------------------------
 for arg in "$@"
 do
-    FILE="${ANDROIDLIB}${arg}"
-
     if [ -d "${MP3LIB}${arg}" ]
     then
-        
-        CHECK="$(adb shell "if [ -d ${FILE} ]; then echo -n "err"; fi")"
-
-        if [ -z "${CHECK}" ]
-        then
-
-            FREE=$(adb shell busybox df     | tail -1 | awk '{print $4}')
-            NEED=$(du "${MP3LIB}${arg}"   | tail -1 | awk '{print $1}')
-        
-            if [ "$FREE" -gt "$NEED" ]
-            then
-                echo "[DEBUG]: Uploading [${arg}] to [${FILE}]"
-                adb push "${MP3LIB}${arg}" "${FILE}"
-            else 
-                echo "[ERROR]: Not enough space available on your device!"
-            fi
-        else
-            echo "[ERROR]: [${arg}] already uploaded!"
-        fi
+        echo -e "[\033[32m:)\033[m] Folder \033[93m${arg}\033[m exists in your library."
     else
-        echo "[ERROR]: No such folder in your library! [${MP3LIB}${arg}]"
+        echo -e "[\033[31m:(\033[m] Folder \033[93m${arg}\033[m doesn't exist in your library!"
+        exit 1
     fi
 done
+
+# Check if there is enough space on the device --------------------------------------
+cd ${MP3LIB}
+NEED=$(du -hcs "${@}" | tail -1 | awk '{print $1}' | sed 's/[^0-9]*//g')
+FREE=$(adb shell busybox df -m | tail -1 | awk '{print $4}')
+
+if [ "$FREE" \> "$NEED" ]
+then
+    echo -e "[\033[32m:)\033[m] Enough Space on the device. Free=$FREE[MB] Needed=$NEED[MB]."
+else
+    echo -e "[\033[31m:(\033[m] Not enough space left. Free=$FREE[MB] Needed=$NEED[MB]."
+    exit 1
+fi
+
+# For each given folder check if its already on the phone
+# If not, upload it
+for arg in "$@"
+do
+    # Assemble paths and escape the android path for the sh shell
+    CURR_FILE_AND="${ANDROIDLIB}${arg}"
+    CURR_FILE_LIB="${MP3LIB}${arg}"
+    FILE="$(echo ${CURR_FILE_AND} | sed 's/ /\\ /g')"
+    CHECK=$(adb shell "if [ -e "${FILE}" ]; then echo -n "err"; else echo -n  "ok"; fi")
+
+    # Only opload if the folder doesn't exist
+    if [ ${CHECK} == "err" ] 
+    then
+        echo -e "[\033[31m:(\033[m] \033[93m${arg}\033[m Already uploaded, skipping!"
+    else
+        echo -e "[\033[32m:)\033[m] \033[93m${arg}\033[m Doesn't exist on phone!"
+        echo -e "[\033[32m:)\033[m] Uploading [${CURR_FILE_LIB}] to [${CURR_FILE_AND}]"
+        echo -e "\033[94m"
+        adb push "${CURR_FILE_LIB}" "${CURR_FILE_AND}"
+        echo -e "\033[m"
+    fi
+        
+done
+
+# Cleanup
+kill `pgrep mp3fs`
+kill `pgrep adb`
